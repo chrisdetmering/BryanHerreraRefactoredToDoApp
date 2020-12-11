082b49fa80b34ca8a358060a41336ed6d63b76e8
Bryan H <bcherrera14@gmail.com>
2020-12-11 07:27:27 -0800

    validate input

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-let todoList = [];
+//let todoList = [];
 let ul = document.getElementById('list');
 
-function addTodo() {
+function validateInput() {
 	let newTodo = document.getElementById('newTodo').value;
-	document.getElementById('newTodo').value = null;
-	todoList.push(newTodo);
-	listItem(newTodo);
-	console.log(newTodo);
-	console.log(todoList);
+	addTodo(newTodo);
+}
+
+function addTodo(newTodo) {
+	if (newTodo === '') {
+		alert('Enter a to do item.');
+		return false;
+	} else {
+		document.getElementById('newTodo').value = null;
+		listItem(newTodo);
+		console.log(newTodo);
+	}
 }
 
 function listItem(todo) {
 	ul = document.getElementById('list');
 	let li = document.createElement('li');
 	let span = document.createElement('span');
+	span.appendChild(document.createTextNode(todo));
 	let deleteButton = document.createElement('button');
 	deleteButton.innerHTML = 'Delete';
 	deleteButton.addEventListener('click', function() {
 		console.log(todo + ' delete');
-		//console.log(deleteButton.parentNode);
 		deleteButton.parentNode.remove();
 	});
-	let doneButton = document.createElement('button');
-	doneButton.innerHTML = 'Done';
-	doneButton.addEventListener('click', function() {
-		console.log(todo + ' done');
-		//console.log(doneButton.previousElementSibling.innerHTML);
-		doneButton.previousElementSibling.innerHTML = '<s>' + doneButton.previousElementSibling.innerHTML + '</s>';
+	let checkDone = document.createElement('input');
+	checkDone.setAttribute('type', 'checkbox');
+	checkDone.addEventListener('change', function() {
+		if (checkDone.checked) {
+			console.log(todo + ' done');
+			checkDone.nextElementSibling.innerHTML = '<s>' + checkDone.nextElementSibling.innerHTML + '</s>';
+		} else {
+			console.log(todo + ' not done');
+			checkDone.nextElementSibling.innerHTML = checkDone.nextElementSibling.innerText;
+		}
 	});
+	li.appendChild(checkDone);
 	li.appendChild(span);
-	span.appendChild(document.createTextNode(todo));
-	li.appendChild(doneButton);
 	li.appendChild(deleteButton);
 	ul.appendChild(li);
 }
