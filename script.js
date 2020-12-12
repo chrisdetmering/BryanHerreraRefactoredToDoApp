eac19ce28e6a28e233b3fd49d8d196adebd268d4
Bryan H <bcherrera14@gmail.com>
2020-12-12 08:32:15 -0800

    style buttons and input

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let todoList = [];
 let ul = document.getElementById('list');
+let uncheckBtn = 'fas fa-circle fa-lg uncheck';
+let checkBtn = 'fas fa-check-circle fa-lg check';
 
 function validateInput() {
 	let newTodo = document.getElementById('newTodo').value;
@@ -18,11 +20,11 @@ function createListElement(todo) {
 	let span = document.createElement('span');
 	span.appendChild(document.createTextNode(todo));
 	let deleteButton = document.createElement('i');
-	deleteButton.className = 'fas fa-times-circle delete-btn';
+	deleteButton.className = 'fas fa-times-circle delete-btn fa-lg';
 	deleteButton.style.visibility = 'hidden';
 	deleteButton.addEventListener('click', todoDelete);
 	let checkDone = document.createElement('i');
-	checkDone.className = 'far fa-circle';
+	checkDone.className = uncheckBtn;
 	checkDone.setAttribute('data-done', 'false');
 	checkDone.addEventListener('click', todoCheck);
 	li.appendChild(checkDone);
@@ -41,17 +43,17 @@ function todoCheck() {
 		console.log(this);
 		this.nextElementSibling.className = 'strike';
 		this.setAttribute('data-done', 'true');
-		this.classList.remove('far');
-		this.classList.remove('fa-circle');
-		this.className = 'fas fa-check-circle';
+		//this.classList.remove('far');
+		//this.classList.remove('fa-circle');
+		this.className = checkBtn;
 		this.nextElementSibling.nextElementSibling.style.visibility = 'visible';
 	} else {
 		console.log('not done');
 		this.nextElementSibling.classList.remove('strike');
 		this.setAttribute('data-done', 'false');
-		this.classList.remove('fas');
-		this.classList.remove('fa-check-circle');
-		this.className = 'far fa-circle';
+		//this.classList.remove('fas');
+		//this.classList.remove('fa-check-circle');
+		this.className = uncheckBtn;
 		this.nextElementSibling.nextElementSibling.style.visibility = 'hidden';
 	}
 }
